d528fa8cbbba59b8dff81ca8a5b5931b3bea6532
RicardoGeek <ricardo@ricardogeek.com>
2018-02-19 13:47:56 -0500

    Fix to table pagination loosing button actions

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,9 @@
 jQuery(function() {
-  
+
+  jQuery("#zones-table").bind('dynatable:afterUpdate', function(e, dynatable) {
+    bindTableActions();
+  });
+
   jQuery("#zones-table").dynatable({
     params: {
       page: 'dtPage'
@@ -28,70 +32,75 @@ jQuery(function() {
     });
   });
 
-  jQuery(".btn-delete-zone").click(function() {
-    var dataId = jQuery(this).attr("data-id");
-    var c = confirm("Are you sure you wan't to do this?");
-    if (c) {
+  jQuery("#save-zone-edits").click(function() {
+    var dataId = jQuery("#edit-zone-id").val();
+    var countryId = jQuery("#edit-ad-country-select").val();
+    var size = jQuery("#edit-ad-size").val();
+    var banner = jQuery("#edit-ad-banner-link").val();
+    var link = jQuery("#edit-ad-link").val();
+
+    jQuery.ajax({
+      type: "post",
+      url: ajaxurl,
+      data: {
+        action: "edit_zone",
+        dataId: dataId,
+        countryId: countryId,
+        size: size,
+        banner: banner,
+        link: link
+      }
+    }).done(function(msg) {
+      var obj = JSON.parse(msg);
+      if (obj === 1) {
+        var me = window.location.href;
+        me = me.split("#");
+        window.location.href = me[0];
+      }
+    });
+  });
+  
+  bindTableActions();
+  
+  //funcitons
+  function bindTableActions() {
+    jQuery(".btn-delete-zone").click(function() {
+      var dataId = jQuery(this).attr("data-id");
+      var c = confirm("Are you sure you wan't to do this?");
+      if (c) {
+        jQuery.ajax({
+          type: "post",
+          url: ajaxurl,
+          data: {
+            id: dataId,
+            action: 'delete_zone'
+          }
+        }).done(function(msg) {
+          alert(msg);
+          window.location.reload();
+        });
+      }
+    });
+
+    jQuery(".btn-edit-zone").click(function() {
+      var dataId = jQuery(this).attr("data-id");
       jQuery.ajax({
         type: "post",
         url: ajaxurl,
         data: {
           id: dataId,
-          action: 'delete_zone'
+          action: 'get_zone'
         }
       }).done(function(msg) {
-        alert(msg);
-        window.location.reload();
+        var zone = JSON.parse(msg);
+        zone = zone[0];
+        jQuery("#edit-zone-id").val(zone.id);
+        jQuery("#edit-ad-country-select").val(zone.countryId);
+        jQuery("#edit-ad-size").val(zone.size);
+        jQuery("#edit-ad-banner-link").val(zone.banner);
+        jQuery("#edit-ad-link").val(zone.link);
       });
-    }
-  });
-
-  jQuery(".btn-edit-zone").click(function() {
-    var dataId = jQuery(this).attr("data-id");
-    jQuery.ajax({
-      type: "post",
-      url: ajaxurl,
-      data: {
-        id: dataId,
-        action: 'get_zone'
-      }
-    }).done(function(msg) {
-      var zone = JSON.parse(msg);
-      zone = zone[0];
-      jQuery("#edit-zone-id").val(zone.id);
-      jQuery("#edit-ad-country-select").val(zone.countryId);
-      jQuery("#edit-ad-size").val(zone.size);
-      jQuery("#edit-ad-banner-link").val(zone.banner);
-      jQuery("#edit-ad-link").val(zone.link);
     });
-  });
-  
-  jQuery("#save-zone-edits").click(function() {
-      var dataId = jQuery("#edit-zone-id").val();
-      var countryId = jQuery("#edit-ad-country-select").val();
-      var size = jQuery("#edit-ad-size").val();
-      var banner = jQuery("#edit-ad-banner-link").val();
-      var link = jQuery("#edit-ad-link").val();
-    
-      jQuery.ajax({
-        type: "post",
-        url: ajaxurl,
-        data: {
-          action: "edit_zone",
-          dataId: dataId,
-          countryId: countryId,
-          size: size,
-          banner: banner,
-          link: link
-        }
-      }).done(function(msg) {
-          var obj = JSON.parse(msg);
-          if(obj === 1) {
-            var me = window.location.href;
-            me = me.split("#");
-            window.location.href = me[0];
-          }
-      });
-  });
-  
+  }
+
 });
